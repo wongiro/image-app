95b521a2875789adc3f807c8c746d5aa8e4c4262
wongiro <sonrobin2018@gmail.com>
2023-02-08 14:12:33 +0300

    added the authenticati on feature for users to sign in

--- image-app/src/App.js
+++ image-app/src/App.js
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Reset from "./pages/Reset";
+import Home from "./pages/Home";
+import { useState } from 'react'
 
 import './App.css';
 
 function App() {
+  const [ isAuth, setIsAuth ] = useState(false);
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={< SignIn />} />
+          <Route path="/" element={< SignIn setIsAuth={ setIsAuth }/>} />
           <Route path="/signup" element={< SignUp />} />
           <Route path="/reset" element={<Reset />}></Route>
+          <Route path="/home" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
